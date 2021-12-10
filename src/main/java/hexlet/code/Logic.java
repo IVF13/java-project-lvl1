92dedda287ab73636d4a0f13928e91939fb8f88d
Ivan <iw.gordienko@yandex.ru>
2021-12-10 18:52:48 +0300

    edit Logic.java for passing checkstyle tests

--- src/main/java/hexlet/code/Logic.java
+++ src/main/java/hexlet/code/Logic.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Logic {
     public static void evenPlay() {
+        final int winsNum = 3;
+        final int lowerNum = 0;
+        final int upperNum = 100;
         int num = 0;
-        int counter = 0;
-        int lowerNum = 0;
-        int upperNum = 100;
         String trueAnswer = "";
         String name = Cli.greet();
 
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        while (counter < 3) {
+        for (int i = 0; i < winsNum; i++) {
 
             num = lowerNum + (int) (Math.random() * upperNum);
             if (num % 2 == 0) {
@@ -29,17 +29,14 @@ public class Logic {
                 String answer = ask.nextLine();
                 if (answer.equals(trueAnswer)) {
                     System.out.println("Correct!");
-                    counter += 1;
                 } else {
-                    System.out.println("'" + answer + "'" + "is wrong answer ;(. Correct answer was " + "'" + trueAnswer + "'.");
+                    System.out.print("'" + answer + "'" + "is wrong answer ;(. ");
+                    System.out.println("Correct answer was " + "'" + trueAnswer + "'.");
                     System.out.println("Let's try again, " + name + "!");
                     System.exit(0);
                 }
             }
         }
-
-        if (counter == 3) {
-            System.out.println("Congratulations, " + name + "!");
-        }
+        System.out.println("Congratulations, " + name + "!");
     }
 }
