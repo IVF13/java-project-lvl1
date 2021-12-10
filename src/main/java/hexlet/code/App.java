1c65409b428e61336ed6e398c92135de4c88c2ad
Ivan <iw.gordienko@yandex.ru>
2021-12-10 18:28:59 +0300

    edit App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
+        System.out.println("2 - Even");
         System.out.println("0 - Exit");
         Scanner choice = new Scanner(System.in);
-        System.out.println("Your choice: ");
+        System.out.print("Your choice: ");
         if (choice.hasNextInt()) {
             int number = choice.nextInt();
             if (number == 1) {
                 Cli.greet();
+            } else if (number == 2) {
+                Logic.evenPlay();
             } else if (number == 0) {
                 System.exit(0);
             }
