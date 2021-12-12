93ee5e5c6fc5d00145df88c017216dcba8a17dd2
Ivan <iw.gordienko@yandex.ru>
2021-12-12 20:50:24 +0300

    edit App.java for third game

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -7,18 +7,36 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         Scanner choice = new Scanner(System.in);
         System.out.print("Your choice: ");
         if (choice.hasNextInt()) {
-            int number = choice.nextInt();
+            switch (choice.nextInt()) {
+                case (1):
+                    Cli.greet();
+                    System.exit(0);
+                case (2):
+                    Logic.evenPlay();
+                    System.exit(0);
+                case (3):
+                    Logic.calculatorPlay();
+                    System.exit(0);
+                case (0):
+                    System.exit(0);
+                default:
+                    System.exit(0);
+            }
+            /*int number = choice.nextInt();
             if (number == 1) {
                 Cli.greet();
             } else if (number == 2) {
                 Logic.evenPlay();
+            } else if (number == 3) {
+                Logic.calculatorPlay();
             } else if (number == 0) {
                 System.exit(0);
-            }
+            }*/
         }
     }
 }
