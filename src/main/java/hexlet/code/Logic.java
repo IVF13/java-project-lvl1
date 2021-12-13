5a22177f415d329b88afe7188dd1fe40aca5fbe4
Ivan <iw.gordienko@yandex.ru>
2021-12-13 15:35:16 +0300

    edit Logic.java and add progression game logic

--- src/main/java/hexlet/code/Logic.java
+++ src/main/java/hexlet/code/Logic.java
@@ -101,4 +101,37 @@ public class Logic {
         }
         System.out.println("Congratulations, " + name + "!");
     }
+
+    public static void aProgression() {
+        final int winsNum = 3;
+        final int arrLenLowerNum = 5;
+        final int arrLenUpperNum = 10;
+        final int randomFirstProgNum = 20;
+        final int randStepUpperNum = 10;
+
+        String name = Cli.greet();
+        System.out.println("What number is missing in the progression?");
+
+        for (int j = 0; j < winsNum; j++) {
+            StringBuilder arr = new StringBuilder();
+            int[] progArr = new int[arrLenLowerNum + (int) (Math.random() * arrLenUpperNum)];
+            int step = 1 + (int) (Math.random() * randStepUpperNum);
+            int hiddenElementNum = (int) (Math.random() * (progArr.length));
+            progArr[0] = (int) (Math.random() * randomFirstProgNum);
+            for (int i = 0; i < progArr.length; i++) {
+                if (i != 0) {
+                    progArr[i] = progArr[i - 1] + step;
+                }
+                if (i == hiddenElementNum) {
+                    arr.append(" .. ");
+                } else {
+                    arr.append(" " + progArr[i] + " ");
+                }
+            }
+            System.out.println("(Correct answer is " + progArr[hiddenElementNum] + ")");
+            toExamineAndCompare(Integer.toString(progArr[hiddenElementNum]), arr.toString(), name);
+            progArr = null;
+        }
+        System.out.println("Congratulations, " + name + "!");
+    }
 }
