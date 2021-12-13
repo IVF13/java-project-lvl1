5797e4617d6f367e8256c79bffb782832dc2e3b1
Ivan <iw.gordienko@yandex.ru>
2021-12-13 14:21:15 +0300

    add GCD game logic

--- src/main/java/hexlet/code/Logic.java
+++ src/main/java/hexlet/code/Logic.java
@@ -3,6 +3,25 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Logic {
+
+    public static void toExamineAndCompare(String trueAnswer, String expression, String name) {
+        System.out.println("Question: " + expression);
+        Scanner ask = new Scanner(System.in);
+        System.out.print("Your answer: ");
+        if (ask.hasNextLine()) {
+            String answer = ask.nextLine();
+            if (answer.equals(trueAnswer)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.print("'" + answer + "'" + "is wrong answer ;(. ");
+                System.out.println("Correct answer was " + "'" + trueAnswer + "'.");
+                System.out.println("Let's try again, " + name + "!");
+                System.exit(0);
+            }
+        }
+
+    }
+
     public static void evenPlay() {
         final int winsNum = 3;
         final int upperNum = 100;
@@ -23,36 +42,19 @@ public class Logic {
         System.out.println("Congratulations, " + name + "!");
     }
 
-    public static void toExamineAndCompare(String trueAnswer, String expression, String name) {
-        System.out.println("Question: " + expression);
-        Scanner ask = new Scanner(System.in);
-        System.out.print("Your answer: ");
-        if (ask.hasNextLine()) {
-            String answer = ask.nextLine();
-            if (answer.equals(trueAnswer)) {
-                System.out.println("Correct!");
-            } else {
-                System.out.print("'" + answer + "'" + "is wrong answer ;(. ");
-                System.out.println("Correct answer was " + "'" + trueAnswer + "'.");
-                System.out.println("Let's try again, " + name + "!");
-                System.exit(0);
-            }
-        }
-
-    }
-
     public static void calculatorPlay() {
         final int winsNum = 3;
         final int signChoice = 4;
         final int firstUpperNum = 20;
         final int secondUpperNum = 20;
-        final int plus = 3;
         final int multiply = 2;
+        final int plus = 3;
         int firstNum;
         int secondNum;
-        String sign = "";
-        String name = Cli.greet();
         int num;
+        String sign = "";
+
+        String name = Cli.greet();
 
         System.out.println("What is the result of the expression?");
         for (int i = 0; i < winsNum; i++) {
@@ -68,8 +70,34 @@ public class Logic {
                 num = firstNum - secondNum;
                 sign = "-";
             }
-            //System.out.println(num);
+            //System.out.println("(Correct answer is " + num + ")");
             toExamineAndCompare(Integer.toString(num), firstNum + " " + sign + " " + secondNum, name);
+        }
+        System.out.println("Congratulations, " + name + "!");
+    }
+
+    public static void gcdPlay() {
+        final int winsNum = 3;
+        final int firstUpperNum = 100;
+        final int secondUpperNum = 100;
+        int firstNum;
+        int secondNum;
+
+        String name = Cli.greet();
+        System.out.println("Find the greatest common divisor of given numbers.");
+
+        for (int i = 0; i < winsNum; i++) {
+            firstNum = (int) (Math.random() * firstUpperNum);
+            secondNum = (int) (Math.random() * secondUpperNum);
+            int a = firstNum;
+            int b = secondNum;
+            while (b != 0) {
+                int tmp = a % b;
+                a = b;
+                b = tmp;
+            }
+            //System.out.println("(Correct answer is " + a + ")");
+            toExamineAndCompare(Integer.toString(a), firstNum + " " + secondNum, name);
         }
         System.out.println("Congratulations, " + name + "!");
     }
