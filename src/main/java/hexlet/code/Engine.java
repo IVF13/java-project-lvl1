0c1c9beafdff3807be3ad29c34108de4451cf41a
Ivan <iw.gordienko@yandex.ru>
2021-12-18 16:13:20 +0300

    rename Logic.java to Engine.java and edit code

---
+++ src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+
+    public static void toExamineAndCompare(String[] answers, String[] expression, String name, int winsNum) {
+        for (int i = 0; i < winsNum; i++) {
+            System.out.println("Question: " + expression[i]);
+            Scanner ask = new Scanner(System.in);
+            System.out.print("Your answer: ");
+            if (ask.hasNextLine()) {
+                String answer = ask.nextLine();
+                if (answer.equals(answers[i])) {
+                    System.out.println("Correct!");
+                } else {
+                    System.out.print("'" + answer + "'" + "is wrong answer ;(. ");
+                    System.out.println("Correct answer was " + "'" + answers[i] + "'.");
+                    System.out.println("Let's try again, " + name + "!");
+                    System.exit(0);
+                }
+            }
+        }
+        System.out.println("Congratulations, " + name + "!");
+    }
+
+
+}
