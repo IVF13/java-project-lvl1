18f8d21f4c597ed91275a856a20395e4a7aa31dc
Ivan <iw.gordienko@yandex.ru>
2021-12-18 16:20:08 +0300

    edit Engine.java

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -2,24 +2,26 @@ package hexlet.code;
 
 import java.util.Scanner;
 
-public class Logic {
+public class Engine {
 
-    public static void toExamineAndCompare(String trueAnswer, String expression, String name) {
-        System.out.println("Question: " + expression);
-        Scanner ask = new Scanner(System.in);
-        System.out.print("Your answer: ");
-        if (ask.hasNextLine()) {
-            String answer = ask.nextLine();
-            if (answer.equals(trueAnswer)) {
-                System.out.println("Correct!");
-            } else {
-                System.out.print("'" + answer + "'" + "is wrong answer ;(. ");
-                System.out.println("Correct answer was " + "'" + trueAnswer + "'.");
-                System.out.println("Let's try again, " + name + "!");
-                System.exit(0);
+    public static void toExamineAndCompare(String[] answers, String[] expression, String name, int winsNum) {
+        for (int i=0; i < winsNum; i++) {
+            System.out.println("Question: " + expression[i]);
+            Scanner ask = new Scanner(System.in);
+            System.out.print("Your answer: ");
+            if (ask.hasNextLine()) {
+                String answer = ask.nextLine();
+                if (answer.equals(answers[i])) {
+                    System.out.println("Correct!");
+                } else {
+                    System.out.print("'" + answer + "'" + "is wrong answer ;(. ");
+                    System.out.println("Correct answer was " + "'" + answers[i] + "'.");
+                    System.out.println("Let's try again, " + name + "!");
+                    System.exit(0);
+                }
             }
         }
-
+        System.out.println("Congratulations, " + name + "!");
     }
 
 
