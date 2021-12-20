5c8ad008ad93a249c2ab1b8aa2653f5ccec262ab
Ivan <iw.gordienko@yandex.ru>
2021-12-20 23:28:55 +0300

    Update Engine.java

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Engine {
 
-    public static void toExamineAndCompare(String[] answers, String[] expression, String name, int winsNum, String condition) {
-        System.out.println(condition);
+    public static void toExamineAndCompare(String[] ans, String[] exp, String name, int winsNum, String question) {
+        System.out.println(question);
         for (int i = 0; i < winsNum; i++) {
-            System.out.println("Question: " + expression[i]);
+            System.out.println("Question: " + exp[i]);
             Scanner ask = new Scanner(System.in);
             System.out.print("Your answer: ");
             if (ask.hasNextLine()) {
                 String answer = ask.nextLine();
-                if (answer.equals(answers[i])) {
+                if (answer.equals(ans[i])) {
                     System.out.println("Correct!");
                 } else {
                     System.out.print("'" + answer + "'" + " is wrong answer ;(. ");
-                    System.out.println("Correct answer was " + "'" + answers[i] + "'.");
+                    System.out.println("Correct answer was " + "'" + ans[i] + "'.");
                     System.out.println("Let's try again, " + name + "!");
                     System.exit(0);
                 }
