17fc62e5215c353de25279e924d687fc8e4490e2
Ivan <iw.gordienko@yandex.ru>
2021-12-20 22:54:26 +0300

    Update Prime.java

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -1,21 +1,22 @@
 package hexlet.code.games;
 
 import hexlet.code.Cli;
+import hexlet.code.Utils;
 import hexlet.code.Engine;
 
 import java.math.BigInteger;
 
 public class Prime {
+    public static final String condition = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
     public static void primePlay(int winsNum) {
         final int randUpperNum = 20;
         String[] generatedNumbers = new String[winsNum];
         String[] answers = new String[winsNum];
 
         String name = Cli.greet();
-        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
 
         for (int i = 0; i < winsNum; i++) {
-            int num = (int) (Math.random() * randUpperNum);
+            int num = Utils.genRandInt(randUpperNum);
             BigInteger bigInteger = BigInteger.valueOf(num);
             boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(num));
 
@@ -29,6 +30,6 @@ public class Prime {
                 generatedNumbers[i] = Integer.toString(num);
             }
         }
-        Engine.toExamineAndCompare(answers, generatedNumbers, name, winsNum);
+        Engine.toExamineAndCompare(answers, generatedNumbers, name, winsNum, condition);
     }
 }
