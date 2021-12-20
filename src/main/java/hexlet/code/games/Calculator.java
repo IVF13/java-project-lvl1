16a975626ec894e96ed141e7e2a6dfb67428b436
Ivan <iw.gordienko@yandex.ru>
2021-12-20 22:53:43 +0300

    Update Calculator.java

--- src/main/java/hexlet/code/games/Calculator.java
+++ src/main/java/hexlet/code/games/Calculator.java
@@ -1,11 +1,14 @@
 package hexlet.code.games;
 
 import hexlet.code.Cli;
+import hexlet.code.Utils;
 import hexlet.code.Engine;
 
+
 public class Calculator {
+    public static final String condition = "What is the result of the expression?";
     public static void calculatorPlay(int winsNum) {
-        final int signChoice = 4;
+        final int signChoiceNum = 4;
         final int firstUpperNum = 20;
         final int secondUpperNum = 20;
         final int multiply = 2;
@@ -16,17 +19,14 @@ public class Calculator {
         String sign = "";
         String[] generatedExpressions = new String[winsNum];
         String[] answers = new String[winsNum];
-
         String name = Cli.greet();
-
-        System.out.println("What is the result of the expression?");
         for (int i = 0; i < winsNum; i++) {
-            firstNum = (int) (Math.random() * firstUpperNum);
-            secondNum = (int) (Math.random() * secondUpperNum);
-            if ((int) (Math.random() * signChoice) == plus) {
+            firstNum = Utils.genRandInt(firstUpperNum);
+            secondNum = Utils.genRandInt(secondUpperNum);
+            if (Utils.genRandInt(signChoiceNum) == plus) {
                 num = firstNum + secondNum;
                 sign = "+";
-            } else if ((int) (Math.random() * signChoice) == multiply) {
+            } else if (Utils.genRandInt(signChoiceNum) == multiply) {
                 num = firstNum * secondNum;
                 sign = "*";
             } else {
@@ -37,6 +37,6 @@ public class Calculator {
             generatedExpressions[i] = firstNum + " " + sign + " " + secondNum;
             //System.out.println("(Correct answer is " + num + ")");
         }
-        Engine.toExamineAndCompare(answers, generatedExpressions, name, winsNum);
+        Engine.toExamineAndCompare(answers, generatedExpressions, name, winsNum, condition);
     }
 }
