fbdee2e9550966ff59b22196ee95e2bb6d4cada4
Ivan <iw.gordienko@yandex.ru>
2021-12-23 17:06:24 +0300

    Update Calculator.java

--- src/main/java/hexlet/code/games/Calculator.java
+++ src/main/java/hexlet/code/games/Calculator.java
@@ -4,39 +4,56 @@ import hexlet.code.Cli;
 import hexlet.code.Utils;
 import hexlet.code.Engine;
 
-
 public class Calculator {
     public static final String QUEST = "What is the result of the expression?";
+
     public static void calculatorPlay(int winsNum) {
-        final int signChoiceNum = 4;
         final int firstUpperNum = 20;
         final int secondUpperNum = 20;
-        final int multiply = 2;
-        final int plus = 3;
-        int firstNum;
-        int secondNum;
         int num;
-        String sign = "";
+
         String[] generatedExpressions = new String[winsNum];
         String[] answers = new String[winsNum];
         String name = Cli.greet();
         for (int i = 0; i < winsNum; i++) {
-            firstNum = Utils.genRandInt(firstUpperNum);
-            secondNum = Utils.genRandInt(secondUpperNum);
-            if (Utils.genRandInt(signChoiceNum) == plus) {
-                num = firstNum + secondNum;
-                sign = "+";
-            } else if (Utils.genRandInt(signChoiceNum) == multiply) {
-                num = firstNum * secondNum;
-                sign = "*";
-            } else {
-                num = firstNum - secondNum;
-                sign = "-";
+            int firstNum = Utils.genRandInt(firstUpperNum);
+            int secondNum = Utils.genRandInt(secondUpperNum);
+            String sign = signChoose();
+
+            switch (sign) {
+                case ("+"):
+                    num = firstNum + secondNum;
+                    break;
+                case ("*"):
+                    num = firstNum * secondNum;
+                    break;
+                case ("-"):
+                    num = firstNum - secondNum;
+                    break;
+                default:
+                    num = 0;
+                    break;
             }
+
             answers[i] = Integer.toString(num);
             generatedExpressions[i] = firstNum + " " + sign + " " + secondNum;
-            //System.out.println("(Correct answer is " + num + ")");
         }
         Engine.toExamineAndCompare(answers, generatedExpressions, name, winsNum, QUEST);
+    }
+
+    private static String signChoose() {
+        final int signChoiceNum = 4;
+        final int multiply = 2;
+        final int plus = 3;
+        String sign = "";
+
+        if (Utils.genRandInt(signChoiceNum) == plus) {
+            sign = "+";
+        } else if (Utils.genRandInt(signChoiceNum) == multiply) {
+            sign = "*";
+        } else {
+            sign = "-";
+        }
+        return sign;
     }
 }
