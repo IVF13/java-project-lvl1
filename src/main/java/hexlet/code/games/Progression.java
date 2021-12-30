3286b6eec9118d86a72f33cf263dae2a911fb683
Ivan <iw.gordienko@yandex.ru>
2021-12-30 17:01:46 +0300

    Update Progression.java

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -1,22 +1,24 @@
 package hexlet.code.games;
 
-import hexlet.code.Cli;
 import hexlet.code.Utils;
 import hexlet.code.Engine;
 
 public class Progression {
     public static final String QUEST = "What number is missing in the progression?";
 
-    public static void aProgressionPlay(int winsNum) {
-        String[] generatedProgressions = new String[winsNum];
+    public static void aProgressionPlay() {
+        final int randFirstProgNum = 20;
+        final int randStepUpperNum = 10;
+        final int arrLenLowerNum = 5;
+        final int arrLenUpperNum = 10;
+        int winsNum = Engine.WINSNUM;
+        String[] generatedProgressionsQuestionSet = new String[winsNum];
         String[] answers = new String[winsNum];
-
-        String name = Cli.greet();
 
         for (int j = 0; j < winsNum; j++) {
             StringBuilder arr = new StringBuilder();
 
-            int[] progArr = progGeneration();
+            int[] progArr = progGen(randFirstProgNum, randStepUpperNum, arrLenLowerNum, arrLenUpperNum);
             int hiddenElementNum = (int) (Math.random() * (progArr.length));
 
             for (int i = 0; i < progArr.length; i++) {
@@ -30,21 +32,16 @@ public class Progression {
                 }
             }
             answers[j] = Integer.toString(progArr[hiddenElementNum]);
-            generatedProgressions[j] = arr.toString();
+            generatedProgressionsQuestionSet[j] = arr.toString();
 
         }
-        Engine.toExamineAndCompare(answers, generatedProgressions, name, winsNum, QUEST);
+        Engine.toExamineAndCompare(answers, generatedProgressionsQuestionSet, QUEST);
     }
 
-    private static int[] progGeneration() {
-        final int randomFirstProgNum = 20;
-        final int randomStepUpperNum = 10;
-        final int arrLenLowerNum = 5;
-        final int arrLenUpperNum = 10;
-
+    private static int[] progGen(int randFirstProgNum, int randStepUpperNum, int arrLenLowerNum, int arrLenUpperNum) {
         int[] progArr = new int[arrLenLowerNum + Utils.genRandInt(arrLenUpperNum)];
-        int step = 1 + Utils.genRandInt(randomStepUpperNum);
-        progArr[0] = Utils.genRandInt(randomFirstProgNum);
+        int step = 1 + Utils.genRandInt(randStepUpperNum);
+        progArr[0] = Utils.genRandInt(randFirstProgNum);
         for (int i = 1; i < progArr.length; i++) {
             progArr[i] = progArr[i - 1] + step;
         }
