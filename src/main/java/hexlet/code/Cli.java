6b3e0776fc54a76682f963e9ed824a71f7fab960
Ivan <iw.gordienko@yandex.ru>
2021-12-30 17:00:11 +0300

    Update Cli.java

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Cli {
     public static String greet() {
         String name = "";
-
         System.out.println("Welcome to the Brain Games!");
         Scanner sc = new Scanner(System.in);
         System.out.print("May I have your name? ");
